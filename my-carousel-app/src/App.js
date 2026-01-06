05ed6433883a08c370b7d5e9ed038f10862ef5d1
diasoce <diasoce@yonsei.ac.kr>
2026-01-06 17:01:03 +0900

    ki

--- my-carousel-app/src/App.js
+++ my-carousel-app/src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import Carousel from './Carousel';  // Carousel 컴포넌트 가져오기
-import './App.css';
+import Carousel from './Carousel';
+import './Carousel.css'; // CSS 연결
 
 function App() {
+  // 보여줄 데이터를 반드시 정의해야 합니다!
+  const data = [
+    { id: 1, title: "여행 1", description: "첫 번째 여행지", content: <div>이미지나 내용</div> },
+    { id: 2, title: "여행 2", description: "두 번째 여행지", content: <div>이미지나 내용</div> },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>여행 정리란</h1>  {/* 예시로 제목 추가 */}
-        <div style={{ height: '600px', position: 'relative' }}>
-          <Carousel
-            baseWidth={330}  // width 대신 baseWidth 사용 (컴포넌트 props에 맞춤)
-            round={true}     // round 활성화
-          />
-        </div>
-      </header>
+    <div style={{ height: '600px', width: '100%', position: 'relative' }}>
+      <Carousel items={data} baseWidth={330} round={true} />
     </div>
   );
 }
-
 export default App;
