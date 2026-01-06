5fae2d57170b08cee23f708ead65684446334d74
diasoce <diasoce@yonsei.ac.kr>
2026-01-06 20:45:53 +0900

    arg

--- my-carousel/vite.config.js
+++ my-carousel/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/voyage/', // GitHub 레포지토리 이름을 여기에 적으세요
 })
