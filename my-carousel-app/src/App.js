0f9a582cce7aeea312355ae40b2491d0cb971180
diasoce <diasoce@yonsei.ac.kr>
2026-01-06 16:34:38 +0900

    ht

--- my-carousel-app/src/App.js
+++ my-carousel-app/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Carousel from './Carousel';  // Carousel 컴포넌트 가져오기
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>여행 정리란</h1>  {/* 예시로 제목 추가 */}
+        <div style={{ height: '600px', position: 'relative' }}>
+          <Carousel
+            baseWidth={330}  // width 대신 baseWidth 사용 (컴포넌트 props에 맞춤)
+            round={true}     // round 활성화
+          />
+        </div>
       </header>
     </div>
   );
