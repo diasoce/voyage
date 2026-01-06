d586e8c18f068b04fde76412ff1b503dfc7cd8f9
diasoce <diasoce@yonsei.ac.kr>
2026-01-07 01:21:27 +0900

    첫고비 넘김

--- my-carousel/src/App.jsx
+++ my-carousel/src/App.jsx
@@ -5,20 +5,20 @@ function App() {
   return (
     <div style={{ 
       position: 'absolute',
-      top: '70%',
-      left: '50%',
+      top: '68%',
+      left: '51%',
       transform: 'translate(-50%, -50%)',
       zIndex: 100,
-      width: '600px',
-      height: '830px',
+      width: '430px',
+      height: '700px',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: 'transparent', // 배경색을 지워야 뒤의 원들이 보입니다
       pointerEvents: 'none'
     }}>
-      <div style={{ width: '600px', height: '830px', pointerEvents: 'auto' }}>
-        <Carousel baseWidth={600} round loop={true} autoplay={true} />
+      <div style={{ width: '430px', height: '700px', pointerEvents: 'auto' }}>
+        <Carousel baseWidth={430} round loop={true} autoplay={true} />
       </div>
     </div>
   );
