28bf7f2acd29e70fd9a0c85e51de1d88eaca7bad
diasoce <diasoce@yonsei.ac.kr>
2026-01-06 17:21:50 +0900

    hy

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Carousel from './Carousel';  // 올바른 경로로 수정
+import Carousel from './Carousel' // 1. 방금 만든 기계를 불러와요
 
 function App() {
   return (
-    <div style={{ height: '600px', position: 'relative' }}>
-      <Carousel baseWidth={330} round={true} />  {/* 하나로 통합, props 추가 */}
+    // 2. 배경 판을 깔아주고
+    <div style={{ height: '600px', position: 'relative', backgroundColor: '#000' }}>
+      {/* 3. 그 위에 회전목마를 올려요! */}
+      <Carousel 
+        width={330} 
+        round // 동그란 모양으로 만들고 싶으면 이걸 써요
+      />
     </div>
   );
 }
-
-export default App;
